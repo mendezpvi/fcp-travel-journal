d873eb74c97902d2cd004eaecd608c609918a297
Vanessa Mendez <182719762+mendezpvi@users.noreply.github.com>
2024-11-26 10:47:33 -0500

    add: script to package.json

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="Footer">
-      <p>Frontend Carrer Path by <a href="https://scrimba.com/courses" target="_blank" rel="noopener noreferrer" className="Footer-link">Scrimba</a> - Developed by <a href="http://" target="_blank" rel="noopener noreferrer" className="Footer-link">VIMP</a></p>
+      <p>Frontend Carrer Path by <a href="https://scrimba.com/courses" target="_blank" rel="noopener noreferrer" className="Footer-link">Scrimba</a> - Developed by <a href="https://github.com/mendezpvi/fcp-travel-journal" target="_blank" rel="noopener noreferrer" className="Footer-link">VIMP</a></p>
     </footer>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'http://mendezpvi.github.io/fcp-travel-journal'
 })
