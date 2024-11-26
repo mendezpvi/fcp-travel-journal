a2fb69dcd00ce5c36c03fccbdcde2f9d96db2a7e
Vanessa Mendez <182719762+mendezpvi@users.noreply.github.com>
2024-11-26 10:49:59 -0500

    fix: base URL

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'http://mendezpvi.github.io/fcp-travel-journal'
+  base: 'https://mendezpvi.github.io/fcp-travel-journal'
 })
